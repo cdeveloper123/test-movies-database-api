4bff0fe2a835a59726d95c67baeaab1e2987f630
cdeveloper123 <cdeveloper123@gmail.com>
2023-12-27 15:34:00 +0530

    Test job: Movies database API

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -67,14 +67,4 @@ class MoviesController < ApplicationController
     def set_active_storage_url_options
       ActiveStorage::Current.url_options = { host: request.host, protocol: request.protocol, port: request.port }
     end
-
-    def pagination_dict(collection)
-      {
-        current_page: collection.current_page,
-        next_page: collection.next_page,
-        prev_page: collection.prev_page,
-        total_pages: collection.total_pages,
-        total_count: collection.total_entries
-      }
-    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,9 +3,20 @@ class Movie < ApplicationRecord
     has_one_attached :poster
     
     validates :title, :poster, presence: true
-    validates :publishing_year, presence: true, numericality: { only_integer: true, message: 'must be a number' }
+    validates :publishing_year, presence: true, numericality: { only_integer: true, less_than_or_equal_to: (Time.current + 5.years).year }
+
+    validate :poster_content_type
+
     
     def poster_url
         poster.blob&.url
+    end
+
+    private
+
+    def poster_content_type
+        if poster.attached? && !poster.content_type.in?(%w(image/jpeg image/jpg image/png))
+            errors.add(:poster, 'must be a JPEG, JPG or PNG')
+        end
     end
 end
